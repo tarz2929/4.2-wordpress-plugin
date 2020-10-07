c8ad9e2f2bc7df24912de3035621526a9f49fc3f
Warren Uhrich <hello@warren.codes>
2020-10-07 14:36:50 -0600

    Initial commit.

---
+++ techcareers-calc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       TECHCareers Calculator
+ * Plugin URI:        https://TECHCareers.CA
+ * Description:       A simple PHP calculator.
+ * Version:           1.0
+ * Requires at least: 5
+ * Requires PHP:      7.4
+ * Author:            Warren Uhrich
+ * Author URI:        https://warren.codes
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
